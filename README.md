a93cfdec4160e9460553fddcf26f83d8058f2448
HighError <hierrorua@gmail.com>
2024-12-05 12:06:04 +0200

    feat: init project

---
+++ README.md
@@ -0,0 +1,21 @@
+# Astro Boilerplate
+
+This is a boilerplate project for Astro.js, designed to provide a quick start for building applications with modern features including TypeScript, Tailwind CSS, and more.
+
+## Features
+
+- **Astro.js**: Latest version of Astro.js for building scalable applications.
+- **TypeScript**: Static type checking for JavaScript.
+- **Tailwind CSS**: Utility-first CSS framework for rapid UI development.
+- **Husky & Lint-Staged**: Pre-commit hooks to ensure code quality.
+- **Biome**: Linter and formatter for JavaScript, TypeScript, and CSS.
+- **Playform Compress**: Image compression for Astro.js.
+- **Compressor**: Minification and compression of CSS and JavaScript files.
+- **Favicons**: Generate favicons for your application.
+- **Robots.txt**: Generate a robots.txt file for your application.
+- **Mail Obfuscation**: Obfuscate email addresses in your application.
+- **Health Check**: Generate a health check endpoint for your application.
+
+## Getting Started
+
+`bunx create-astro-app -e https://github.com/Eviloma/boilerplates/tree/master/astro`
---
+++ astro.config.mjs
@@ -0,0 +1,67 @@
+// @ts-check
+import { defineConfig, envField } from "astro/config";
+
+import node from "@astrojs/node";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import playformCompress from "@playform/compress";
+import compressor from "astro-compressor";
+import devtoolBreakpoints from "astro-devtool-breakpoints";
+import favicons from "astro-favicons";
+import healthcheckTs from "astro-healthcheck-ts";
+import icon from "astro-icon";
+import lenis from "astro-lenis";
+import mailObfuscation from "astro-mail-obfuscation";
+import metaTags from "astro-meta-tags";
+import robotsTxt from "astro-robots-txt";
+import vtbot from "astro-vtbot";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "http://localhost:4321",
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap(),
+    lenis(),
+    icon(),
+    robotsTxt(),
+    vtbot({
+      loadingIndicator: false,
+    }),
+    mailObfuscation(),
+    favicons({
+      appName: "Astro Boilerplate",
+      appShortName: "Astro",
+      appDescription: "Astro Boilerplate",
+      faviconsDarkMode: false,
+    }),
+    devtoolBreakpoints(),
+    metaTags(),
+    healthcheckTs(),
+    playformCompress(),
+    compressor(),
+  ],
+
+  env: {
+    validateSecrets: true,
+    schema: {
+      G_ANALYTICS_TAG: envField.string({
+        access: "secret",
+        context: "server",
+        optional: true,
+      }),
+      G_TAG_MANAGER: envField.string({
+        access: "secret",
+        context: "server",
+        optional: true,
+      }),
+    },
+  },
+});
